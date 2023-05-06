30553d31abcade057487e45d7275bd6e89e16461
İdil Bölükbaşı <idilblks@gmail.com>
2023-05-06 14:57:00 +0300

    project edited

--- LibraryProject/Models/ChangePasswordModel.cs
+++ LibraryProject/Models/ChangePasswordModel.cs
@@ -3,8 +3,8 @@
     public class ChangePasswordModel
     {
         public string Username { get; set; }
-        public string OldPassword { get; set; }
-        public string NewPassword { get; set; }
+        public string OldPassword { get; set; }   
+        public string NewPassword { get; set; } 
     }
 
 }
